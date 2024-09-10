4fe49d69432d7e75833dc33614e318ea1baa7fc9
NureAlam68 <nurealam151068@gmail.com>
2024-09-11 00:26:11 +0600

    task one

--- task-one.js
+++ task-one.js
@@ -7,9 +7,9 @@ function calculateTax(income, expenses) {
     if(income < 0 || expenses < 0 || income < expenses) {
         return  "Invalid Input";
     }
-    const remainingTaka = income - expenses;
-    const tax = remainingTaka * .20;
+    const profit = income - expenses;
+    const tax = profit * .20;
     return tax;
 }
-let result = calculateTax(10000, 3000);
+let result = calculateTax(444, 78);
 console.log(result);
